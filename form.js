59a40a12d521cafed41073939cf2a2a8069daadb
Void <peze95@hotmail.it>
2021-07-20 14:04:28 +0200

    boh

--- form.js
+++ form.js
@@ -1,2 +1,17 @@
-email = document.getElementById("exampleInputEmail1") 
-console.log(email)
+const email = document.getElementById("exampleInputEmail1") 
+const password = document.getElementById("exampleInputPassword1")
+const form = document.getElementById("form")
+const error = document.getElementById("error")
+
+form.addEventListener("submit", (e) => {
+    let messages = []
+
+    if(email.value === "" || email.value === null)
+        messages.push("Email is required")
+
+    if(messages.length > 0)
+        e.preventDefault()
+        error.innerText = messages.join()
+        document.getElementById("error").style.color = '#d00'
+
+})
