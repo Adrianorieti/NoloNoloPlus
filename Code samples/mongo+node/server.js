07a2322d1e1bb5a387564ed36144002dc55db2ba
Void <peze95@hotmail.it>
2021-07-22 18:56:44 +0200

    FINALLY

--- Code samples/mongo+node/server.js
+++ Code samples/mongo+node/server.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const user = require('./moduledb1');
-
-mongoose.connect('mongodb+srv://Adriano:123Alienware$!$@cluster0.5ajuv.mongodb.net/Prova', { useNewUrlParser: true , useUnifiedTopology: true })
-
-app.use(bodyParser.urlencoded({extended: true}));
+var url ='mongodb+srv://Adriano:<password>@cluster0.5ajuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect("mongodb+srv://Adriano:123Alienware$!$@cluster0.5ajuv.mongodb.net/Prova?retryWrites=true&w=majority", { useNewUrlParser: true ,  useUnifiedTopology: true  });
+app.use(express.json());
 
 app.get('/', function(req, res) {
-    res.sendFile('/home/void/Desktop/Git_project/NoloNoloPlus/Code samples/mongo+node/node+ajax-client.html');
+	res.sendFile('/home/void/Desktop/Git_project/NoloNoloPlus/Code samples/mongo+node/client.html');
 })
 
 app.post('/', function(req, res){
+
     let newUser = new user({
         name: req.body.name,
         surname: req.body.surname,
         age: req.body.age
-    })
-    res.send("Correctly received");
+    });
+    console.log(req.body.name);
+    console.log(req.body.surname);
+    console.log(req.body.age);
     newUser.save();
     res.redirect('/');
 })
-
-
 
 app.listen(8000, function(){
     console.log('Server is running on port 8000')
