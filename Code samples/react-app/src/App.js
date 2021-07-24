00b891c76effb5968c396092caae7b87c98352b4
Void <peze95@hotmail.it>
2021-07-24 15:47:17 +0200

    prova react

--- Code samples/react-app/src/App.js
+++ Code samples/react-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Prova  from './funzione-prova';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <div className="container">
+       <div className="insert">
+          <input type="text" placeholder="Inserisci un brano" />
+
+            <Prova />   
+
+       </div>
+      </div>
+    </section>
   );
 }
 
