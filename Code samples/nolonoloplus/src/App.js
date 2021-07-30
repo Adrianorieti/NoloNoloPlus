24c6dafbe26b786ec13d970ad667ec7547168170
Miki Spigarolo <miki.miao@gmail.com>
2021-07-30 22:21:36 +0200

    Aggiunta banner e adeguamenti carousel

--- Code samples/nolonoloplus/src/App.js
+++ Code samples/nolonoloplus/src/App.js
@@ -9,42 +9,44 @@ class App extends React.Component {
   render (){
 
     return(
-     <div> 
+    <div> 
       <nav className="navbar navbar-expand-lg navbar-success bg-success justify-content-end">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">NoloNolo</a>
-        <button className="navbar-toggler custom-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon "></span> 
-        </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav" > 
-          <ul className="navbar-nav ">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Products</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Login/Register</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#" tabIndex="-1" >Contacts</a>
-            </li>
-          </ul>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">NoloNolo</a>
+          <button className="navbar-toggler custom-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon "></span> 
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav" > 
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Products</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Login/Register</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" tabIndex="-1" >Contacts</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-      
+      </nav>
+      <div className="banner-image"></div>
+
       <div className="bg-image" >
-
-
-<center><h1><font face="Arial" color="white">Travelling with us you can smell the world</font></h1></center>
       </div>
+      
+
       <div>
         <CarouselContainer />
       </div>
- 
-      </div>
+
+        
+      
+    </div>
   );
 
   }
--- Code samples/nolonoloplus/src/component.js
+++ Code samples/nolonoloplus/src/component.js
@@ -9,36 +9,36 @@ const CarouselContainer = () =>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://mdbootstrap.com/img/new/slides/041.jpg"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/strada-viaggiare-1528810575.jpg?resize=480:*"
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h3>Oscal Wilde</h3>
+      <p>Vivi la vita senza scuse, viaggia senza rimpianti.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://mdbootstrap.com/img/new/slides/041.jpg"
+      src="https://i.redd.it/2o4r27jok2g41.jpg"
       alt="Second slide"
     />
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h3>Confucio</h3>
+      <p>Ovunque tu vada, vacci con tutto il tuo cuore.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://mdbootstrap.com/img/new/slides/041.jpg"
+      src="https://www.gelestatic.it/thimg/oylBPO0HdAXlNmT0WxAomVhSx8c=/fit-in/960x540/https%3A//www.lastampa.it/image/contentid/policy%3A1.33824702%3A1560675768/191b5ef0-6681-11e8-8f8f-753ca73ba52f_WPNX-29290_sudafrica-kDBF-U104175853522304-1024x576%40LaStampa.it.jpg%3Ff%3Ddetail_558%26h%3D720%26w%3D1280%26%24p%24f%24h%24w%3Dfd59bc1"
       alt="Third slide"
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3>Cesare Pavese</h3>
+      <p>Non si ricordano i giorni, si ricordano gli attimi.</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
