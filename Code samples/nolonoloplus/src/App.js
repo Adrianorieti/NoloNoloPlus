12da4b6ec705aee7be42665665690cb180f8ee58
anubis09 <lorenzotozzi98@gmail.com>
2021-08-05 18:27:55 +0200

    Added Footer

--- Code samples/nolonoloplus/src/App.js
+++ Code samples/nolonoloplus/src/App.js
@@ -4,54 +4,57 @@ import treehd from './images/treehd.jpg'
 import CarouselContainer from './components/Carousel';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import RentForm from './components/RentForm';
 import LoginPage from './pages/LoginPage';
 import RegisterForm from './components/Register';
+import Footer from './components/Footer';
 
 class App extends React.Component {
 
 
-  render (){
+  render() {
 
-    return(
-    <div> 
-
-      <Navbar logoName="NoloNoloPlus" />
-
-      <Switch>
-
-      <Route path="/" exact>
-      
-
-
-      <div className="bg-image" >
-      
-      <Banner />
-  
-    
-      <RentForm />
-     
-      </div>
-
+    return (
       <div>
-        <CarouselContainer />
+
+        <Navbar logoName="NoloNoloPlus" />
+
+        <Switch>
+
+          <Route path="/" exact>
+
+
+
+            <div className="bg-image" >
+
+              <Banner />
+
+
+              <RentForm />
+
+            </div>
+
+            <div>
+              <CarouselContainer />
+            </div>
+
+          </Route>
+
+          <Route path="/login">
+            <div className="bg-image loginpage" >
+              <LoginPage />
+            </div>
+
+          </Route>
+
+        </Switch>
+        <Footer companyName="NoloNoloPlus" prod1="Biciclette" prod2="Monopattini"
+          locality="Bologna, BO 40121, IT" mail="info@nolonolo.it" number="+39 051 268 80"
+          copyright="NOLONOLOPLUS SRL INCAZZATISSIMI" />
+
       </div>
-
-      </Route>
-
-      <Route path="/login">
-      <div className="bg-image loginpage" >
-        <LoginPage/>
-      </div>
-
-      </Route>
-
-      </Switch>
-        
-      
-    </div>
-  );
+    );
 
   }
 }
