4c6e79b97f00002e618bf0839a299bd13cbb1592
Void <peze95@hotmail.it>
2021-08-08 18:03:43 +0200

    added validation login

--- Code samples/nolonolobackend/server.js
+++ Code samples/nolonolobackend/server.js
@@ -6,10 +6,11 @@ var ObjectId = require('mongoose').Types.ObjectId;
 const app = express();
 var cors = require('cors');
 const bcrypt = require('bcrypt');
+const { PRIORITY_ABOVE_NORMAL } = require('constants');
 
-var url ='mongodb+srv://Adriano:123Armadiopieno$!$@cluster0.5ajuv.mongodb.net/Nolo?retryWrites=true&w=majority';
+var url = 'mongodb+srv://Adriano:123Armadiopieno$!$@cluster0.5ajuv.mongodb.net/Nolo?retryWrites=true&w=majority';
 
-mongoose.connect(url, { useNewUrlParser: true ,  useUnifiedTopology: true  });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 
@@ -17,26 +18,26 @@ app.use(cors());
 //     res.header("Access-Control-Allow-Origin", "*");
 //     next();
 //   });
-  
+
 
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('<p>Hello WOrld</p>');
-	//res.sendFile('/home/void/Desktop/Git_project/NoloNoloPlus/Code samples/mongo+node/client2.html');
+    //res.sendFile('/home/void/Desktop/Git_project/NoloNoloPlus/Code samples/mongo+node/client2.html');
     //res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.post('/register', async (req, res) => {
 
-        const mail = req.body.email;
-        
-        //guardo se c'è già una mail registrata
-        const source = await user.findOne({email: mail });
+    const mail = req.body.email;
 
-        if (!(source)) //se non c'è una copia della mail nel db
-        {
+    //guardo se c'è già una mail registrata
+    const source = await user.findOne({ email: mail });
+
+    if (!(source)) //se non c'è una copia della mail nel db
+    {
         //la password arriva in base64
         console.log(req.body.password);
         const password = req.body.password;
@@ -47,38 +48,56 @@ app.post('/register', async (req, res) => {
         console.log(decodedpass);
 
         //qui faccio l'hash della password con sale
-        const hash = await bcrypt.hash(decodedpass, 10, function(err, hash) {
-            
-        let newUser = new user({
-            name: req.body.name,
-            surname: req.body.surname,
-            phone: req.body.phone,
-            email: req.body.email,
-            password: hash
-        });
-        //salviamo in mongodb
-        newUser.save();
+        const hash = await bcrypt.hash(decodedpass, 10, function (err, hash) {
+
+            let newUser = new user({
+                name: req.body.name,
+                surname: req.body.surname,
+                phone: req.body.phone,
+                email: req.body.email,
+                password: hash
+            });
+            //salviamo in mongodb
+            newUser.save();
 
         })
-         }else {
-           console.log(' Errore esiste già la mail');
-           res.status(500).send({error: 'Mail already exists'}); 
-        }
-    });
-       
-    
-
-/*
-app.post('/login', async (req, res) => 
-{
-    try
-    {
-        const email = req.body.email;
-        const password = req.body.password;
-    } catch(e)
-    {}
+    } else {
+        console.log(' Errore esiste già la mail');
+        res.status(500).send({ error: 'Mail already exists' });
+    }
 });
-*/
+
+
+
+app.post('/login', async (req, res) => {
+    const mail = req.body.email;
+    const source = await user.findOne({ email: mail });
+    console.log(source);
+    if (source) {
+
+        const password = req.body.password;
+        //qui la decodifico
+        const buff = Buffer.from(password, 'base64');
+        const decodedpass = buff.toString('utf-8');
+        console.log(decodedpass);
+        console.log(source.password);
+        //utilizzo compare di bcrypt per comparare la password in plain text e il suo ipotetico hash
+        //ci riesce perchè ha uno schema di cifratura che glielo permette da quanto ho capito
+        if ( await bcrypt.compare(decodedpass, source.password) )
+        {
+            console.log("Success");
+        }
+        else
+        {
+            console.log("Password doesn't match");
+        }
+    }
+    else {
+        console.log('Errore la mail non esiste');
+        res.status(500).send({ error: 'Mail not exists' });
+    }
+})
+
 
 /*
 app.delete('/:id', function(req, res) {
@@ -103,7 +122,6 @@ user.findOne({age: {$gte:40} }, function (err, docs) {
 });
 
  */
-
-app.listen(8000, function(){
+app.listen(8000, function () {
     console.log('Server is running on port 8000')
 })
--- Code samples/nolonoloplus/src/pages/LoginPage.js
+++ Code samples/nolonoloplus/src/pages/LoginPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-class LoginPage extends React.Component
-{
+class LoginPage extends React.Component {
   state = {
     firstName: "",
     secondName: "",
@@ -23,13 +22,12 @@ class LoginPage extends React.Component
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  doAjax = () =>
-  {
+  doAjax = () => {
     //faccio l'encoding della password in base64 perchè così non ho problemi con caratteri strani
     const buff = Buffer.from(this.state.password, 'utf-8');
-    const encodedpass = buff.toString('base64'); 
-    
-    
+    const encodedpass = buff.toString('base64');
+
+
     //creo il json che rappresenta lo schema del database con i dati  
     var obj = `{
       "name": "${this.state.firstName}" ,
@@ -38,191 +36,222 @@ class LoginPage extends React.Component
       "email": "${this.state.email}",
       "password": "${encodedpass}"
    }`;
-   //creo l'oggetto ajax per la post
-       var xhr = new XMLHttpRequest();
-       xhr.open("POST", "http://localhost:8000/register", true);
-       xhr.setRequestHeader('Content-Type', 'application/json');
-       xhr.onload =  function() {
-           if (xhr.status == 200)
-           {
-               //console.log(this.responseText);
-               console.log("daiiiiiiiiiiiiiiiiiiiiii");
-           }
-           else if(xhr.status == 500)
-            {
-              console.log("La mail esiste già");
-              document.getElementById('mail-error').innerHTML = "Mail already in use BOOMER";
-            }
-       }
-       xhr.onerror = function() {
-           console.log(this.response);
-           console.log("Error ....");
-       }
-       xhr.send(obj);
+    //creo l'oggetto ajax per la post
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8000/register", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        //console.log(this.responseText);
+        console.log("daiiiiiiiiiiiiiiiiiiiiii");
+      }
+      else if (xhr.status == 500) {
+        console.log("La mail esiste già");
+        document.getElementById('mail-error').innerHTML = "Mail already in use BOOMER";
+      }
+    }
+    xhr.onerror = function () {
+      console.log(this.response);
+      console.log("Error ....");
+    }
+    xhr.send(obj);
   };
 
-/*Valida l'input per vedere se ci sono problemi,  BETA VERSION */
-  validate = () => 
-  {
+  /*Valida l'input per vedere se ci sono problemi,  BETA VERSION */
+  validate = () => {
     let count = 0;
     let firstnameError = '';
     let secondnameError = '';
     let phoneError = '';
-    let passwordError= '';
+    let passwordError = '';
     let emailError = '';
-    let repeatpassError= '';
-    var paswd= '/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/ ';
-    var mailformat = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ';
+    let repeatpassError = '';
+    const paswd = '/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/ ';
+    const mailformat = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ';
 
-    if(this.state.firstName === '')
-    {
+    if (this.state.firstName === '') {
       firstnameError = 'Please fill out this space';
-      this.setState({firstnameError});
+      this.setState({ firstnameError });
       count++;
     }
-    if(this.state.secondName === '')
-    {
+    if (this.state.secondName === '') {
       secondnameError = 'Please fill out this space';
-      this.setState({secondnameError});
+      this.setState({ secondnameError });
       count++;
     }
-      //devo trovare un altro modo per capire se l'input è o no un numero
-    if( this.state.phone === '' || this.state.phone.length != 10)
-    {
+    //devo trovare un altro modo per capire se l'input è o no un numero
+    if (this.state.phone === '' || this.state.phone.length != 10) {
       phoneError = "Please insert phone correctly";
-      this.setState({phoneError});
+      this.setState({ phoneError });
       count++;
     }
-    if(this.state.password === '' || this.state.password.match(paswd) )
-    {
+    if (this.state.password === '' || this.state.password.match(paswd)) {
       passwordError = "Password must contain a number and a symbol and a length between 7 to 15 characters";
-      this.setState({passwordError});
+      this.setState({ passwordError });
       count++;
     }
-    if(this.state.email === '' || this.state.email.match(mailformat))
-    {
+    if (this.state.email === '' || this.state.email.match(mailformat)) {
       emailError = "Email syntax is not valid";
-      this.setState({emailError});
+      this.setState({ emailError });
       count++;
     }
-    if(this.state.repeatPassword != this.state.password)
-    {
+    if (this.state.repeatPassword != this.state.password) {
       repeatpassError = " Passwords don't matches";
-      this.setState({repeatpassError});
+      this.setState({ repeatpassError });
       count++;
     }
 
-    if(count > 0)
+    if (count > 0)
       return false;
     else
       return true;
   }
 
   /* Handler che entra in gioco quando il pulsante di register è premuto*/
-  handleRegister  = event => 
-  {
+  handleRegister = event => {
     event.preventDefault();
-    if(this.validate())
-    {
+    if (this.validate()) {
       console.log("Eseguito");
       this.doAjax();
     }
   };
 
-
-
-    render()
-    {
-      return(
-    
-    <div className="login" scrolling="auto">
-        <ul className="nav nav-tabs" id="myTab" role="tablist">
-        <li className="nav-item" role="presentation">
-          <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Login</button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Register</button>
-        </li>
-       
-      </ul>
-
-      <div className="tab-content" id="myTabContent">
-
-        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-          <div>
-            <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input  onChange={this.handleChange} type="email" className="form-control" id="loginemail" aria-describedby="emailHelp" />
-            </div>
-            <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">Password</label>
-              <input onChange={this.handleChange} type="password" className="form-control" id="loginpassword" />
-            </div>
-            <div className="mb-3 form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-              <label className="form-check-label" for="exampleCheck1">Remember me</label>
-            </div>
-            <button onClick={this.handleLogin} type="submit" className="btn btn-primary">Login</button>
-          </div>
-    
-    
-        </div>
-    
-        <div className="tab-pane fade register" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-    
-          <div>
-            <div className="mb-3">
-              <label for="firstName" className="form-label">First Name</label>
-              <input onChange={this.handleChange} type="text" className="form-control" name="firstName" aria-describedby="emailHelp" placeholder="John Doe" />
-            </div>
-            <div style={{fontSize: 12, color: 'red'}}>{this.state.nameError}</div>
-            <div className="mb-3">
-              <label for="secondName" className="form-label">Second name</label>
-              <input onChange={this.handleChange} type="text" className="form-control" name="secondName" aria-describedby="emailHelp" placeholder="John Doe" />
-            </div>
-
-            <div style={{fontSize: 12, color: 'red'}}>{this.state.secondnameError}</div>
-
-            <div className="mb-3">
-              <label for="email" className="form-label">Email</label>
-              <input  onChange={this.handleChange} type="email" className="form-control" name="email" placeholder="diocane@studio.unibo.it" />
-            </div>
-
-            <div id='mail-error' style={{fontSize: 12, color: 'red'}}>{this.state.emailError}</div>
-            
-            <div className="mb-3">
-              <label for="phone" className="form-label">Phone Number</label>
-              <input onChange={this.handleChange} type="text" className="form-control" name="phone" />
-            </div>
-
-            <div style={{fontSize: 12, color: 'red'}}>{this.state.phoneError}</div>
-
-            <div className="mb-3">
-              <label for="password" className="form-label">Password</label>
-              <input onChange={this.handleChange} type="password" className="form-control" name="password" />
-            </div>
-            <div style={{fontSize: 12, color: 'red'}}>{this.state.passwordError}</div>
-
-            <div className="mb-3">
-              <label for="repeatPassword" className="form-label">Repeat Password</label>
-              <input  onChange={this.handleChange} type="password" className="form-control" name="repeatPassword" />
-            </div>
-            <div style={{fontSize: 12, color: 'red'}}>{this.state.repeatpassError}</div>
-
-            <div className="mb-3 form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-              <label className="form-check-label" for="exampleCheck1">Remember me</label>
-            </div>
-            <button onClick={this.handleRegister} type="submit" className="btn btn-primary">Register</button>
-          </div>
-    
-    
-        </div>
-      </div>
-
-      </div>
+  handleLogin = event => {
+    event.preventDefault();
+    const paswd = '/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/ ';
+    const mailformat = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ';
+    if ((this.state.email === '' || this.state.email.match(mailformat)) && (this.state.password === '' || this.state.password.match(paswd)))
+     {
       
-     );
+        console.log('basta fare il coglione e inserisci le cose');
+      
+      } else { 
+
+      const buff = Buffer.from(this.state.password, 'utf-8');
+      const encodedpass = buff.toString('base64');
+      const obj = `{
+        "email": "${this.state.email}",
+        "password": "${encodedpass}"
+      }`;
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:8000/login", true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onload = function () {
+        if (xhr.status == 200) {
+          //console.log(this.responseText);
+          console.log("Logged in correctly");
+        }
+        else if (xhr.status == 500) {
+          
+          document.getElementById('loginmail-error').innerHTML = "Mail or password is wrong !";
+
+        }
+      }
+      xhr.onerror = function () {
+        console.log(this.response);
+        console.log("Error ....");
+      }
+      xhr.send(obj);
+    }
+  }
+  
+  
+
+
+  render() {
+    return (
+
+      <div className="login" scrolling="auto">
+        <ul className="nav nav-tabs" id="myTab" role="tablist">
+          <li className="nav-item" role="presentation">
+            <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Login</button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Register</button>
+          </li>
+
+        </ul>
+
+        <div className="tab-content" id="myTabContent">
+
+          <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <div>
+              <div className="mb-3">
+                <label for="loginemail" className="form-label">Email address</label>
+                <input onChange={this.handleChange} type="email" className="form-control" name="email" id="loginemail" aria-describedby="emailHelp" aria-required="true" />
+                <label id='loginmail-error' for="loginemail" style={{ fontSize: 12, color: 'red' }}></label>
+
+              </div>
+              <div className="mb-3">
+                <label for="loginpassword" className="form-label">Password</label>
+                <input onChange={this.handleChange} type="password" className="form-control" name="password" id="loginpassword" aria-required="true" />
+              </div>
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                <label className="form-check-label" for="exampleCheck1">Remember me</label>
+              </div>
+              <input onClick={this.handleLogin} type="submit" className="btn btn-primary" value="Login"></input>
+            </div>
+
+
+          </div>
+
+          <div className="tab-pane fade register" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+
+            <div>
+              <div className="mb-3">
+                <label for="firstName" className="form-label">First Name</label>
+                <input onChange={this.handleChange} id="firstName" type="text" className="form-control" name="firstName"
+                  aria-describedby="emailHelp" placeholder="John" aria-required="true" />
+                <label for="firstName" style={{ fontSize: 12, color: 'red' }}>{this.state.firstnameError}</label>
+              </div>
+
+              <div className="mb-3">
+                <label for="secondName" className="form-label">Second name</label>
+                <input onChange={this.handleChange} id="secondName" type="text" className="form-control" name="secondName"
+                  aria-describedby="emailHelp" placeholder="Doe" aria-required="true" />
+                <label for="secondName" style={{ fontSize: 12, color: 'red' }}>{this.state.secondnameError}</label>
+              </div>
+
+              <div className="mb-3">
+                <label for="email" className="form-label">Email</label>
+                <input onChange={this.handleChange} id="email" type="email" className="form-control" name="email"
+                  placeholder="username@studio.unibo.it" aria-required="true" />
+                <label id='mail-error' for="email" style={{ fontSize: 12, color: 'red' }}>{this.state.emailError}</label>
+              </div>
+
+              <div className="mb-3">
+                <label for="phone" className="form-label">Phone Number</label>
+                <input onChange={this.handleChange} id="phone" type="text" className="form-control" name="phone" aria-required="true" />
+                <label for="phone" style={{ fontSize: 12, color: 'red' }}>{this.state.phoneError}</label>
+              </div>
+
+              <div className="mb-3">
+                <label for="password" className="form-label">Password</label>
+                <input onChange={this.handleChange} id="password" type="password" className="form-control" name="password" aria-required="true" />
+                <label for="password" style={{ fontSize: 12, color: 'red' }}>{this.state.passwordError}</label>
+              </div>
+
+              <div className="mb-3">
+                <label for="repeatPassword" className="form-label">Repeat Password</label>
+                <input onChange={this.handleChange} id="repeatPassword" type="password" className="form-control" name="repeatPassword" aria-required="true" />
+                <label for="repeatPassword" style={{ fontSize: 12, color: 'red' }}>{this.state.repeatpassError}</label>
+              </div>
+
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                <label className="form-check-label" for="exampleCheck1">Remember me</label>
+              </div>
+
+              <input onClick={this.handleRegister} type="submit" className="btn btn-primary" value="Register" ></input>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    );
   }
 }
 export default LoginPage;
